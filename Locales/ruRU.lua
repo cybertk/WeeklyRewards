b340ff1a9df270079c20dfc6afbae26013760e2c
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-02-04 18:45:02 +0500

    Update ruRU.lua

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -4,26 +4,26 @@ end
 
 local _, ns = ...
 local L = ns.L
---Translator ZamestoTV
+
 -- Binding
 L["BINDING_NAME_WeeklyRewards"] = "Показать/скрыть окно WeeklyRewards"
 
 -- Minimap tooltip
-L["minimap_tooltip_left_click"] = "|cff00ff00ЛКМ|r — открыть WeeklyRewards."
-L["minimap_tooltip_drag"] = "|cff00ff00Перетаскивание|r — переместить иконку"
+L["minimap_tooltip_left_click"] = "|cff00ff00ЛКМ|r - открыть WeeklyRewards."
+L["minimap_tooltip_drag"] = "|cff00ff00Перетаскивание|r - переместить иконку"
 L["minimap_tooltip_locked"] = " |cffff0000(заблокировано)|r"
 
 -- Window titlebar buttons
 L["close_button_tooltip"] = "Закрыть окно"
 L["settings_button_tooltip"] = "Настройки"
-L["settings_button_description"] = "Настроим всё по вашему вкусу"
+L["settings_button_description"] = "Давайте немного изменим настройки"
 L["characters_button_tooltip"] = "Персонажи"
-L["characters_button_description"] = "Включить/отключить ваших персонажей."
-L["characters_button_remove_hint"] = "<Ctrl+клик для удаления персонажа>"
+L["characters_button_description"] = "Вкл./откл. Ваших персонажей."
+L["characters_button_remove_hint"] = "<Ctrl+ЛКМ - удалить персонажа>"
 L["columns_button_tooltip"] = "Колонки"
-L["columns_button_description"] = "Включить/отключить отслеживание наград."
+L["columns_button_description"] = "Вкл./откл. отслеживание наград."
 L["sort_button_tooltip"] = "Сортировка"
-L["sort_button_description"] = "Выберите параметры сортировки для колонок наград."
+L["sort_button_description"] = "Выберите параметры сортировки для столбцов с наградами."
 
 -- Settings menu
 L["settings_show_minimap_button"] = "Показывать кнопку у миникарты"
@@ -37,20 +37,24 @@ L["settings_max_width_percent"] = "% от полного экрана"
 L["settings_background_color"] = "Цвет фона"
 L["settings_show_border"] = "Показывать рамку"
 L["settings_utility_title"] = "Утилиты"
-L["settings_auto_untrack_quests"] = "Авто-снятие заданий"
-L["settings_auto_untrack_quests_tooltip"] = "При входе в игру снимает все задания, управляемые WeeklyRewards — для чистоты панели заданий"
+L["settings_auto_untrack_quests"] = "Отключение заданий"
+L["settings_auto_untrack_quests_tooltip"] = "Отключить отслеживание всех заданий, управляемых WeeklyRewards, при входе в игру, что позволит сделать панель журнала заданий более удобной."
 L["settings_broadcast_rewards"] = "Оповещать о наградах"
-L["settings_broadcast_rewards_tooltip"] = "После получения наград Отряда — отправит сводку в чат с наибольшей аудиторией (автовыбор канала)."
+L["settings_broadcast_rewards_tooltip"] = "После получения наград Отряда Вы сможете опубликовать сводку по ней. Для этого автоматически выбирается канал чата с наибольшей аудиторией."
 
 -- Table columns
-L["column_realm"] = "Сервер"
+L["column_realm"] = "Игровой мир"
 L["column_covenant"] = "Ковенант"
 L["column_location"] = "Местоположение"
 L["column_last_update"] = "Обновлено"
 
+-- Covenant tooltips
+L["covenant_sanctum_unknown"] = "Статус обновления Обители неизвестен"
+L["covenant_not_joined"] = "Этот персонаж не вступал ни в один Ковенант"
+
 -- Table tooltips
-L["table_sort_hint"] = "<ЛКМ — сортировка>"
-L["table_reset_progress_hint"] = "<Ctrl+клик — сброс прогресса>"
+L["table_sort_hint"] = "<ЛКМ - сортировка>"
+L["table_reset_progress_hint"] = "<Ctrl+ЛКМ - сброс прогресса>"
 
 -- Progress tooltips
 L["progress_rewards_received_at"] = "Награды получены:"
@@ -62,6 +66,11 @@ L["progress_drops"] = "Дроп"
 -- Reward tooltips
 L["reward_rewards"] = "Награды"
 L["reward_time_left"] = "Осталось времени: "
+
+-- Summary
+L["summary_completed_count"] = "%s выполнено %d раз на этой неделе"
+L["summary_progress"] = "Прогресс Отряда:"
+L["summary_rewards_received"] = "Всего получено наград: "
 
 -- Sorting options
 L["sort_reward_group"] = "Группа наград"
