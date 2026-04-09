d8bc97855a2103c584e474a78ffbd15aa8f3e458
Quanlong <kyan.ql.he@gmail.com>
2026-04-09 17:11:32 +0300

    feat: more Midnight weekly rewards
    - Emissary of War

--- DB/BonusEvent.lua
+++ DB/BonusEvent.lua
@@ -8,12 +8,14 @@ namespace.DB.rewardCandidiates["bonus"] = {
 			.. "|n|nThese events rotate on a set schedule, including:"
 			.. "|n- {quest:93593}" -- A Call to Battle
 			.. "|n- {quest:93595}" -- A Call to Delves
+			.. "|n- {quest:93598}" -- Emissary of War
 			.. "",
 		minimumLevel = 90,
 		timeLeft = C_DateAndTime.GetSecondsUntilWeeklyReset,
 		entries = {
 			{ quest = 93593 }, -- A Call to Battle
 			{ quest = 93595, unlockAura = 471521 }, -- A Call to Delves
+			{ quest = 93598, unlockAura = 225787 }, -- Emissary of War
 		},
 	},
 }
