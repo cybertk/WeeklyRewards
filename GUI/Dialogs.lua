de8886512501f90f6e6bb3a9e34e30e9315e0d03
Quanlong <kyan.ql.he@gmail.com>
2026-06-16 18:53:34 +0300

    feat: allow multi-line notes

--- GUI/Dialogs.lua
+++ GUI/Dialogs.lua
@@ -3,25 +3,50 @@ local addonName, namespace = ...
 local L = namespace.L
 local Util = namespace.Util
 
+local EditBoxNumLines
+local function StaticPopup_MultiLineEditBoxOnShow(editBox)
+	editBox:SetMultiLine(true)
+	editBox:SetTextInsets(6, 6, 6, 24)
+
+	EditBoxNumLines = -1
+end
+
+local function StaticPopup_MultiLineEditBoxOnTextChanged(editBox)
+	local numLines = editBox:GetNumLines()
+
+	if EditBoxNumLines == numLines then
+		return
+	end
+
+	EditBoxNumLines = numLines
+	StaticPopup_ResizeShownDialogs()
+end
+
+local function StaticPopup_MultiLineEditBoxOnEnterPressed(editBox)
+	editBox:Insert("\n")
+end
+
 local CHARACTER_NOTE = {
 	text = SET_FRIENDNOTE_LABEL,
 	button1 = OKAY,
 	button2 = CANCEL,
 	hasEditBox = true,
 	editBoxWidth = 350,
-	maxLetters = 48,
+	maxLetters = 3000,
 	OnShow = function(dialog, character)
 		local editBox = dialog:GetEditBox()
+
 		editBox:SetText(character:GetNote())
 		editBox:SetFocus()
+
+		StaticPopup_MultiLineEditBoxOnShow(editBox)
 	end,
 	OnAccept = function(dialog, character)
 		character:SetNote(dialog:GetEditBox():GetText())
 		namespace.GUIMain:Redraw()
 	end,
-	EditBoxOnEnterPressed = function(editBox, data)
-		editBox:GetParent():GetButton1():Click()
-	end,
+	EditBoxOnTextChanged = StaticPopup_MultiLineEditBoxOnTextChanged,
+	EditBoxOnEnterPressed = StaticPopup_MultiLineEditBoxOnEnterPressed,
 	EditBoxOnEscapePressed = StaticPopup_StandardEditBoxOnEscapePressed,
 	timeout = 0,
 	whileDead = 1,
