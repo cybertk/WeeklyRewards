492907d63e6723e71f958e83c28209a8dd6f56a8
Quanlong <kyan.ql.he@gmail.com>
2026-09-11 23:07:23 +0800

    feat: paginate the characters dropdown menu

---
+++ GUI/CharactersButton.lua
@@ -0,0 +1,70 @@
+local _, namespace = ...
+
+local L = namespace.L
+local Util = namespace.Util
+local CharacterStore = namespace.CharacterStore
+local Settings = namespace.Settings
+local Utils = namespace.Utils
+
+WeeklyRewardsCharactersButtonMixin = {}
+
+function WeeklyRewardsCharactersButtonMixin:OnLoad()
+	Settings:SetupPagedMenu(self, function(_, rootMenu)
+		local characterStore = CharacterStore.Get()
+		rootMenu:CreateTitle():AddInitializer(function(frame)
+			frame.fontString:SetText(
+				format(
+					"|cnWHITE_FONT_COLOR:%s (%d/%d)|r",
+					L["characters_button_title"],
+					characterStore:GetNumEnabledCharacters(),
+					characterStore:GetNumCharacters()
+				)
+			)
+		end)
+
+		local pageSize = 30
+		Settings:CreatePagedMenu(rootMenu, characterStore:ForEach(nop, CharacterStore.IsNotCurrentPlayer), function(character)
+			local checkbox = rootMenu:CreateCheckbox(character:GetNameInClassColor(), function()
+				return character.enabled or false
+			end)
+
+			checkbox:SetResponder(function()
+				if IsControlKeyDown() then
+					self:RemoveCharacter(character)
+					return MenuResponse.Refresh
+				else
+					character.enabled = not character.enabled
+					namespace.GUIMain:Redraw()
+					return MenuResponse.Refresh
+				end
+			end)
+		end, self, pageSize)
+
+		rootMenu:CreateSpacer()
+		rootMenu:CreateTitle(GREEN_FONT_COLOR:WrapTextInColorCode(L["characters_button_remove_hint"]))
+	end)
+end
+
+function WeeklyRewardsCharactersButtonMixin:RemoveCharacter(character)
+	Util:Debug("Removing character:", character.name)
+
+	StaticPopup_ShowGenericConfirmation(CONFIRM_COMPACT_UNIT_FRAME_PROFILE_DELETION:format(character:GetNameInClassColor()), function()
+		CharacterStore.Get():RemoveCharacter(character.GUID)
+		namespace.GUIMain:Redraw()
+	end)
+end
+
+function WeeklyRewardsCharactersButtonMixin:OnEnter()
+	self.Icon:SetVertexColor(0.9, 0.9, 0.9, 1)
+	Utils:SetBackgroundColor(self, 1, 1, 1, 0.05)
+	namespace.GUIMain:SetTooltipOwner(GameTooltip, self)
+	GameTooltip:SetText(L["characters_button_title"], 1, 1, 1, 1, true)
+	GameTooltip:AddLine(L["characters_button_description"], NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b)
+	GameTooltip:Show()
+end
+
+function WeeklyRewardsCharactersButtonMixin:OnLeave()
+	self.Icon:SetVertexColor(0.7, 0.7, 0.7, 1)
+	Utils:SetBackgroundColor(self, 1, 1, 1, 0)
+	GameTooltip:Hide()
+end
