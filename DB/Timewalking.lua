6a49988958685c0d4f2d0ac74230cc29d3a2f4ea
Quanlong <kyan.ql.he@gmail.com>
2026-09-19 21:02:05 +0300

    perf: remove outdated progress to reduce SavedVariables size

--- DB/Timewalking.lua
+++ DB/Timewalking.lua
@@ -24,7 +24,6 @@ namespace.DB.rewardCandidiates["timewalking"] = {
 		description = "Timewalking Dungeon",
 		group = RewardsGroup.TIMEWALKING_EVENTS,
 		minimumLevel = 1,
-		rollover = true,
 		entries = {
 			{ quest = 83285, items = { 225348 }, unlockAura = 452307 }, -- [Classic] - [Quest] The Ancient Scroll - [Aura] Sign of the Past
 			{ quest = 40168, items = { 129747 }, unlockAura = 335148 }, -- [TBC] - [Quest] The Swirling Vial - [Aura] Sign of the Twisting Nether
